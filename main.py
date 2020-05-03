f77999cc72a66b63e2f0d4140ce375e78e3f06b0
Томилов Станислав Игорьевич <Tomilov.S.Igo@sberbank.ru>
2020-05-03 13:05:09 +0300

    added following channels: CA, crypto_solyanka_tca_bot, crypto_solyanka102_bot, cryptosolyanka_waveanalysis_bot, crypto_solyanka101_bot, crypto_solyanka_x100_bot

--- main.py
+++ main.py
@@ -8,25 +8,31 @@ api_hash = '88db0805d24311ea0e8f05e2115aa1a2'
 # Getting a connection to Telegram account
 app = Client('my-session', phone_number = phone_number, api_id = api_id, api_hash = api_hash)
 
+
 counter = 1
 divider = 1
 
 crypto_solyanka_dict = {
                         -384882468: -1001442805574,         #test
-                        -1001442805574: -1001442805574, #TelegramTraders
-                        940152572: -100142914981,       #crypto_solyanka_cryptoangel_bot
-                        964236548: -1001471225365,      #crypto_solyanka_german_bot
-                        725595486: -1001443655853,      #crypto_solyanka_rose_bot
-                        969227740: -1001294788098,      #crypto_solyanka_cointrade_bot
-                        725420715: -1001403623279,      #crypto_solyanka_james_bot
-                        902178497: -1001341953633,      #crypto_solyanka_wcg_bot
-                        954117019: -1001193842012,      #crypto_solyanka_artem_bot
-                        854070442: -1001167690912,      #crypto_solyanka_pirate_bot
-                        981862130: -1001457275719,      #crypto_solyanka_marginwhales_bot
-                        909453054: -1001354472158,      #crypto_solyanka_asian_whales_bot
-                        928204131: -1001341569964,      #crypto_solyanka_coincoach_bot
-                        863139972: -1001233068606,      #crypto_solyanka_crypto_coach_bot
-                        938258245: -1001412736228       #crypto_solyanka_palm_bot
+                        -1001442805574: -1001442805574, #TelegramTraders                    #
+                        940152572: -1001429149813,      #crypto_solyanka_cryptoangel_bot    #Crypto Angel
+                        964236548: -1001471225365,      #crypto_solyanka_german_bot         #Герман на блокчейне
+                        725595486: -1001443655853,      #crypto_solyanka_rose_bot           #Rose
+                        969227740: -1001294788098,      #crypto_solyanka_cointrade_bot      #Coin Trade
+                        725420715: -1001403623279,      #crypto_solyanka_james_bot          #James Rich
+                        902178497: -1001341953633,      #crypto_solyanka_wcg_bot            #WCG
+                        954117019: -1001193842012,      #crypto_solyanka_artem_bot          #Artem Trade
+                        854070442: -1001167690912,      #crypto_solyanka_pirate_bot         #Pirate VIP Club 2.0
+                        981862130: -1001457275719,      #crypto_solyanka_marginwhales_bot   #Margin Whales
+                        909453054: -1001354472158,      #crypto_solyanka_asian_whales_bot   #Азиатские Киты
+                        928204131: -1001341569964,      #crypto_solyanka_coincoach_bot      #Coin coach
+                        863139972: -1001233068606,      #crypto_solyanka_crypto_coach_bot   #Trading Crypto Coach
+                        938258245: -1001412736228,      #crypto_solyanka_palm_bot           #Palm Venice Beach
+                        703814867: -1001445146705,      #crypto_solyanka_tca_bot            #TCA Exclusive                              -
+                        1050648215: -1001406749095,     #crypto_solyanka102_bot             #Chaos Trade
+                        1068800127: -1001320562354,     #cryptosolyanka_waveanalysis_bot    #Волновой анализ
+                        817128313: -1001469264645,      #crypto_solyanka101_bot             #CryptoBull
+                        986428578: -1001360819912       #crypto_solyanka_x100_bot           #x100. Богатейший Ди
                         }
 
 telegram_trader_news_dict = {
